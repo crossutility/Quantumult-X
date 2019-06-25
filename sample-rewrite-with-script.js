cbde3f4f86ca4acd21f647193684510a9cf0be7f
Cross Utility <44658120+crossutility@users.noreply.github.com>
2019-06-25 10:11:08 +0800

    Update sample-rewrite-with-script.js

--- sample-rewrite-with-script.js
+++ sample-rewrite-with-script.js
@@ -1,10 +1,11 @@
+// $request, $response, $notify(title, message), console.log(message)
+// $request.scheme, $request.method, $request.url, $request.path, $request.body, $request.headers
+// $response.statusCode, $response.headers, $response.body
+
 var body = $response.body;
 var obj = JSON.parse(body);
 
 obj['result'] = 0;
 body = JSON.stringify(obj);
-
-// console.log(body);
-// $notify('This is title', 'This is message');
 
 $done(body);
