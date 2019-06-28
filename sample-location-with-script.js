7534cb7aa7f4287f78afe8cf50019d14a5c7808e
Cross Utility <44658120+crossutility@users.noreply.github.com>
2019-06-28 19:55:54 +0800

    Create sample-location-with-script.js

---
+++ sample-location-with-script.js
@@ -0,0 +1,11 @@
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['isp'];
+var ip = obj['query'];
+
+$done({title, subtitle, ip});
