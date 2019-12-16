602b5ca00b037db3ddb4c2ace7f9446344ed93c4
Cross Utility <44658120+crossutility@users.noreply.github.com>
2019-12-16 14:02:52 +0800

    Create sample-task.js

---
+++ sample-task.js
@@ -0,0 +1,30 @@
+
+/**
+ * @fileoverview Example to compose HTTP reqeuest
+ * and handle the response. Same task shares context.
+ * $task.fetch() only works for tasks not rewrite
+ * rules.
+ *
+ * @supported Quantumult X (v1.0.5-build166)
+ */
+
+var url = "https://example.com/";
+var method = "POST";
+var headers = {"Field": "test-header-param"};
+var data = {"info": "abc"};
+
+var myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("Title", "Subtitle", response.body); // Success!
+}, reason => {
+    // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+});
