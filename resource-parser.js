9e24a6e3fc3579508838af6a05e4f093372be2e5
Cross Utility <44658120+crossutility@users.noreply.github.com>
2020-04-14 13:56:31 +0800

    Create resource-parser.js

---
+++ resource-parser.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview Example to parse the resource to the format of Quantumult X.
+ *
+ * @supported Quantumult X (v1.0.8-build253)
+ */
+
+
+// $resource, $notify(title, subtitle, message)
+// HTTP reqeust and persistent storage related APIs are not supported in resource parser.
+
+// $resource.link contains the original URL of the resource or the path if the resource is local.
+// $resource.content contains the response(UTF8) from the URL .
+
+// $done({error : "error description"});
+// $done({content : "the modified content"});
+
+var sampleA = "shadowsocks=ui-a.example.com:80, method=chacha20, password=pwd, obfs=http, obfs-host=bing.com, obfs-uri=/resource/file, fast-open=false, udp-relay=false, tag=Tag-A";
+var sampleB = "shadowsocks=ui-b.example.com:80, method=chacha20, password=pwd, obfs=http, obfs-host=bing.com, obfs-uri=/resource/file, fast-open=false, udp-relay=false, tag=Tag-B";
+var total = sampleA + "\n" + sampleB;
+$done({content : total});
