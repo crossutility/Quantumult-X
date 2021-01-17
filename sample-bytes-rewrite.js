3c1e334aa0e16d93b154119e17d027d18bcc2db1
Cross Utility <44658120+crossutility@users.noreply.github.com>
2021-01-17 21:42:45 +0800

    Create sample-bytes-rewrite.js

---
+++ sample-bytes-rewrite.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Example of HTTP rewrite of raw data.
+ * You can modify the original buffer then call $done with the modified result.
+ * You can create a new buffer data then $done with the result.
+ * You can replace the original buffer with the binary data fetched from another URL.
+ *
+ * The below rewrite sample specifies the process to replace the bytes of an image with the bytes of another image.
+ * It is worth noting that it's simpler to use the 302 redirection rewrite, but this is a good example on the usage of modifing the body that is not a string.
+ *
+ * @supported Quantumult X (v1.0.19-build480) and iOS 14.0 +
+ *
+ * $response.bodyBytes is instance of ArrayBuffer.
+ *
+ * [rewrite_local]
+ * ^https://raw\.githubusercontent\.com/crossutility/Quantumult-X/master/icon-samples/apple-rbk\.PNG url script-response-body https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-bytes-rewrite.js
+ */
+
+
+ const url = "https://raw.githubusercontent.com/crossutility/Quantumult-X/master/icon-samples/telegram-rbk.PNG";
+ const myRequest = {
+     url: url
+ };
+
+ $task.fetch(myRequest).then(response => {
+     $done({bodyBytes: response.bodyBytes});
+ }, reason => {
+     $done();
+ });
+
+
+// var originalBuffer = $response.bodyBytes;
+// var buffer = new ArrayBuffer(8);
+// var uint8 = new Uint8Array(buffer);
+// uint8[0] = 3;
+// uint8[7] = 5;
+
+// $done({bodyBytes: buffer});
