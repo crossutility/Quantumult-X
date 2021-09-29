98342a7ee6a1370f2245c8883e162c92097d7817
Cross Utility <44658120+crossutility@users.noreply.github.com>
2021-09-29 12:43:38 +0800

    Create sample-fetch-opts-policy.js

---
+++ sample-fetch-opts-policy.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview Example to send HTTPS request through a specific policy or server.
+ * It is worth noting that $task.fetch request with specific policy option will lose the optimization of connection reuse,
+ * It means that every HTTPS request with the specific policy option will has the complete TCP handshake and TLS handshake,
+ * This option is only recommended for some special usecases such as server info(geo location) checking.
+ *
+* @supported Quantumult X (v1.0.25-build598)
+ */
+
+ var url = "http://ip-api.com/json"
+ var opts = {
+     policy: "direct"
+ };
+ var myRequest = {
+     url: url,
+     opts: opts
+ };
+
+ $task.fetch(myRequest).then(response => {
+     console.log(response.body);
+     $done();
+ }, reason => {
+     $done();
+ });
+
+ // For cusomized geo location checker with event-interaction task, you can use HTTPS API instead of HTTP API (built-in geo_location_checker only supports HTTP API).
+ // The event-interaction task only works when Quantumult X Tunnel is running.
+ // The img-url for event-interaction also works with UIAction if it is a SF Symbol.
+ // [task_local]
+ // event-interaction geo_location.js, tag=GeoIP, img-url=text.magnifyingglass.system
+
+ // The geo_location.js
+ // The $environment.params is auto passed in when user triggered a UIAction and is related to the configuration element. Right now only suppots the server tag.
+ //
+ // var url = "http://ip-api.com/json"
+ // var opts = {
+ //     policy: $environment.params
+ // };
+ // var myRequest = {
+ //     url: url,
+ //     opts: opts
+ // };
+ //
+ // $task.fetch(myRequest).then(response => {
+ //     $done({"title":"The Title","message":response.body});
+ // }, reason => {
+ //     $done({"title":"The Error Title","message":"The message"});
+ // });
